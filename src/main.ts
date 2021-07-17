4ce1c754522559810942de71bcf48fe5c411731e
kidatsy <toshiro.kida@gmail.com>
2021-07-17 13:09:27 -0700

    adding dotenv and PORT env var for use w/ heroku

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
